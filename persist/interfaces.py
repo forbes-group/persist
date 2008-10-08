454875d67af3ef21c19d5684826cf2ca10eacd70
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2008-10-08 13:38:48 -0700

    [svn r2019] Interfaces files.
    --HG--
    branch : utils

---
+++ persist/interfaces.py
@@ -0,0 +1,22 @@
+"""Various Interfaces."""
+__all__ = ['Interface','IArchivable']
+
+class Interface(object):        # pragma: no cover 
+    """This is a stub allowing the insertion later of an interface
+    class.  An interface defines some properties and methods but
+    maintains no data and should not be instantiated."""
+
+class IArchivable(Interface):   # pragma: no cover 
+    """Interface for objects that suport archiving."""
+    def archive_1(self,env=None):
+        """Return (rep,args,imports).
+        
+        Defines a representation rep of the instance self where the
+        instance can be reconstructed from the string rep evaluated in
+        the context of args with the specified imports = list of
+        (module,iname,uiname) where one has either "import module as
+        uiname", "from module import iname" or "from module import
+        iname as uiname".
+        """
+        raise NotImplementedError
+
