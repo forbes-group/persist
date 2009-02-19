95abc581e429fce257aa90f239b7de83855b951f
Michael McNeil Forbes <michael.forbes+bitbucket@gmail.com>
2009-02-19 13:47:27 -0800

    [svn r2140] Allow objects_ to get default documentation from other objects.
    --HG--
    branch : utils

--- persist/interfaces.py
+++ persist/interfaces.py
@@ -1,10 +1,17 @@
-"""Various Interfaces."""
-__all__ = ['Interface','IArchivable']
+r"""Various Interfaces."""
+__all__ = ['Interface', 'IArchivable', 'implements']
 
-class Interface(object):        # pragma: no cover 
-    """This is a stub allowing the insertion later of an interface
-    class.  An interface defines some properties and methods but
-    maintains no data and should not be instantiated."""
+try:
+    from zope.interface import Interface
+    from zope.interface import implements
+    
+except ImportError:
+    class Interface(zope.interface.Interface):        # pragma: no cover 
+        """This is a stub allowing the insertion later of an interface
+        class.  An interface defines some properties and methods but
+        maintains no data and should not be instantiated."""
+    def implements(interface):          # pragma: no cover
+        pass
 
 class IArchivable(Interface):   # pragma: no cover 
     """Interface for objects that suport archiving."""
